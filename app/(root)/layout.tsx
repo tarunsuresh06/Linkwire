b09ec7555e638db549655ed69a9d339153783d62
tarunsuresh06 <tarunsuresh.v@gmail.com>
2024-03-11 21:24:05 +0530

    Add Initial Nav Bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/shared/Header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main className="w-full min-h-screen">{children}</main>;
+  return (
+    <main className="w-full min-h-screen">
+      <Header />
+      {children}
+    </main>
+  );
 }
---
+++ components/shared/Header.tsx
@@ -0,0 +1,91 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import { headerLinks } from "@/constants";
+
+const CustomLink = ({ href, title, className = "" }: any) => {
+  const path = usePathname();
+  return (
+    <Link href={href} className={`${className} relative group font-medium`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-primary absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] duration-300 
+          ${path === href ? "w-full" : "w-0"}`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="w-full bg-black/5">
+      <div className="wrapper flex justify-between items-center">
+        <Link href="/" className="text-2xl font-semibold">
+          Linkwire.
+        </Link>
+        <nav className="hidden md:block">
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/search" title="Search" className="mx-4" />
+          <CustomLink href="/posts" title="Posts" className="ml-4" />
+        </nav>
+        <div className="flex justify-center items-center">
+          <Link
+            href="/signin"
+            className="bg-blue-400 hover:bg-blue-500 text-white px-5 py-2 rounded-lg hidden md:inline-block font-semibold duration-100"
+          >
+            Sign In
+          </Link>
+          <Sheet>
+            <SheetTrigger className="md:hidden hover:scale-110 duration-100">
+              <GiHamburgerMenu size={30} />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-2xl text-left">
+                  Linkwire.
+                </SheetTitle>
+                <SheetDescription></SheetDescription>
+              </SheetHeader>
+              <nav className="flex flex-col justify-center items-start text-primary mt-4">
+                {headerLinks.map((link) => (
+                  <Link
+                    key={link.route}
+                    href={link.route}
+                    className="w-full hover:bg-gray-100 my-1 text-lg p-2 rounded-md duration-100"
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </nav>
+              <Separator />
+              <div className="mt-4 w-full border border-solid">
+                <Link
+                  href="/signin"
+                  className="bg-blue-400 hover:bg-blue-500 text-white rounded py-2 px-4 block text-center font-medium"
+                >
+                  Sign In
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
