950c33ee5cd2cd7af4a8b7922ab1142e283da859
tarunsuresh06 <tarunsuresh.v@gmail.com>
2024-03-11 15:18:52 +0530

    Initial Commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main className="w-full min-h-screen">{children}</main>;
+}
---
+++ constants/index.ts
@@ -0,0 +1,35 @@
+// API Status Constants
+export const apiStatusConstants = {
+  initial: "INITIAL",
+  success: "SUCCESS",
+  failure: "FAILURE",
+  inProgress: "IN_PROGRESS",
+};
+
+// Nav Header Links and Routes
+export const headerLinks = [
+  { label: "Home", route: "/" },
+  { label: "Search", route: "/search" },
+  { label: "Groups", route: "/groups" },
+  { label: "Channels", route: "/channels" },
+];
+
+// Category List
+export const categoryList = [
+  { name: "Adult", value: "adult" },
+  { name: "Art and Creativity", value: "art_creativity" },
+  { name: "Automotive", value: "automotive" },
+  { name: "Business and Finance", value: "business_finance" },
+  { name: "Cryptocurrency and Finance", value: "cryptocurrency_finance" },
+  { name: "Education", value: "education" },
+  { name: "Entertainment", value: "entertainment" },
+  { name: "Gaming", value: "gaming" },
+  { name: "Health and Fitness", value: "health_fitness" },
+  { name: "Hobbies and Interests", value: "hobbies_interests" },
+  { name: "Lifestyle", value: "lifestyle" },
+  { name: "Music", value: "music" },
+  { name: "News and Current Affairs", value: "news_current_affairs" },
+  { name: "Science and Technology", value: "science_technology" },
+  { name: "Sports", value: "sports" },
+  { name: "Travel", value: "travel" },
+];
